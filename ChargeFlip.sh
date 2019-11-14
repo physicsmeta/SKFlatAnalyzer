15f9cd21a6b5fb668796c71fa7972696d5bc8c9d
Hooooon12 <theking461@snu.ac.kr>
2019-11-14 17:08:05 +0900

    clean and IDv2 updated on ChargeFlip.sh

--- ChargeFlip.sh
+++ ChargeFlip.sh
@@ -11,56 +11,72 @@
 
 #python python/SKFlat.py -a ChargeFlipValidation -y 2016 -i DYJets -n 50 --userflags CFrate &
 
-python python/SKFlat.py -a ChargeFlipValidation -y 2016 -i DYJets -n 50 --userflags CFrate,OnlyTwo &
+#python python/SKFlat.py -a ChargeFlipValidation -y 2016 -i DYJets -n 50 --userflags CFrate,OnlyTwo &
+
+
+
 
 #CFrate, ClosureTest, HalfSampleTest first run (to calculate the Even set CFrate)
 
-#python python/SKFlat.py -a ChargeFlip -y 2016 -i DYJets -n 50 --userflags CFrate &
-#python python/SKFlat.py -a ChargeFlip -y 2016 -i DYJets_Pt-100To250 -n 50 --userflags CFrate &
-#python python/SKFlat.py -a ChargeFlip -y 2016 -i DYJets_Pt-250To400 -n 50 --userflags CFrate &
-#python python/SKFlat.py -a ChargeFlip -y 2016 -i DYJets_Pt-400To650 -n 50 --userflags CFrate &
-#python python/SKFlat.py -a ChargeFlip -y 2016 -i DYJets_Pt-650ToInf -n 50 --userflags CFrate &
-#python python/SKFlat.py -a ChargeFlip -y 2016 -i TTLL_powheg -n 50 --userflags CFrate &
-#python python/SKFlat.py -a ChargeFlip -y 2017 -i DYJets -n 50 --userflags CFrate &
-#python python/SKFlat.py -a ChargeFlip -y 2017 -i TTLL_powheg -n 50 --userflags CFrate &
-#python python/SKFlat.py -a ChargeFlip -y 2018 -i DYJets_MG -n 50 --userflags CFrate &
-#python python/SKFlat.py -a ChargeFlip -y 2018 -i TTLL_powheg -n 50 --userflags CFrate &
+#python python/SKFlat.py -a ChargeFlip_IDv2 -y 2016 -i DYJets -n 50 --userflags CFrate &
+#python python/SKFlat.py -a ChargeFlip_IDv2 -y 2016 -i TTLL_powheg -n 50 --userflags CFrate &
+python python/SKFlat.py -a ChargeFlip_IDv2 -y 2017 -i DYJets -n 50 --userflags CFrate &
+python python/SKFlat.py -a ChargeFlip_IDv2 -y 2017 -i TTLL_powheg -n 50 --userflags CFrate &
+python python/SKFlat.py -a ChargeFlip_IDv2 -y 2018 -i DYJets_MG -n 50 --userflags CFrate &
+python python/SKFlat.py -a ChargeFlip_IDv2 -y 2018 -i TTLL_powheg -n 50 --userflags CFrate &
 #
-#python python/SKFlat.py -a ChargeFlip -y 2016 -i DYJets -n 50 --userflags ClosureTest &
-#python python/SKFlat.py -a ChargeFlip -y 2016 -i DYJets_Pt-100To250 -n 50 --userflags ClosureTest &
-#python python/SKFlat.py -a ChargeFlip -y 2016 -i DYJets_Pt-250To400 -n 50 --userflags ClosureTest &
-#python python/SKFlat.py -a ChargeFlip -y 2016 -i DYJets_Pt-400To650 -n 50 --userflags ClosureTest &
-#python python/SKFlat.py -a ChargeFlip -y 2016 -i DYJets_Pt-650ToInf -n 50 --userflags ClosureTest &
-#python python/SKFlat.py -a ChargeFlip -y 2016 -i TTLL_powheg -n 50 --userflags ClosureTest &
-#python python/SKFlat.py -a ChargeFlip -y 2017 -i DYJets -n 50 --userflags ClosureTest &
-#python python/SKFlat.py -a ChargeFlip -y 2017 -i TTLL_powheg -n 50 --userflags ClosureTest &
-#python python/SKFlat.py -a ChargeFlip -y 2018 -i DYJets_MG -n 50 --userflags ClosureTest &
-#python python/SKFlat.py -a ChargeFlip -y 2018 -i TTLL_powheg -n 50 --userflags ClosureTest &
+#python python/SKFlat.py -a ChargeFlip_IDv2 -y 2016 -i DYJets -n 50 --userflags ClosureTest &
+#python python/SKFlat.py -a ChargeFlip_IDv2 -y 2016 -i TTLL_powheg -n 50 --userflags ClosureTest &
+#python python/SKFlat.py -a ChargeFlip_IDv2 -y 2017 -i DYJets -n 50 --userflags ClosureTest &
+#python python/SKFlat.py -a ChargeFlip_IDv2 -y 2017 -i TTLL_powheg -n 50 --userflags ClosureTest &
+#python python/SKFlat.py -a ChargeFlip_IDv2 -y 2018 -i DYJets_MG -n 50 --userflags ClosureTest &
+#python python/SKFlat.py -a ChargeFlip_IDv2 -y 2018 -i TTLL_powheg -n 50 --userflags ClosureTest &
 #
-#python python/SKFlat.py -a ChargeFlip -y 2016 -i DYJets -n 50 --userflags HalfSampleTest &
-#python python/SKFlat.py -a ChargeFlip -y 2016 -i DYJets_Pt-100To250 -n 50 --userflags HalfSampleTest &
-#python python/SKFlat.py -a ChargeFlip -y 2016 -i DYJets_Pt-250To400 -n 50 --userflags HalfSampleTest &
-#python python/SKFlat.py -a ChargeFlip -y 2016 -i DYJets_Pt-400To650 -n 50 --userflags HalfSampleTest &
-#python python/SKFlat.py -a ChargeFlip -y 2016 -i DYJets_Pt-650ToInf -n 50 --userflags HalfSampleTest &
-#python python/SKFlat.py -a ChargeFlip -y 2016 -i TTLL_powheg -n 50 --userflags HalfSampleTest &
-#python python/SKFlat.py -a ChargeFlip -y 2017 -i DYJets -n 50 --userflags HalfSampleTest &
-#python python/SKFlat.py -a ChargeFlip -y 2017 -i TTLL_powheg -n 50 --userflags HalfSampleTest &
-#python python/SKFlat.py -a ChargeFlip -y 2018 -i DYJets_MG -n 50 --userflags HalfSampleTest &
-#python python/SKFlat.py -a ChargeFlip -y 2018 -i TTLL_powheg -n 50 --userflags HalfSampleTest &
+#python python/SKFlat.py -a ChargeFlip_IDv2 -y 2016 -i DYJets -n 50 --userflags HalfSampleTest &
+#python python/SKFlat.py -a ChargeFlip_IDv2 -y 2016 -i TTLL_powheg -n 50 --userflags HalfSampleTest &
+#python python/SKFlat.py -a ChargeFlip_IDv2 -y 2017 -i DYJets -n 50 --userflags HalfSampleTest &
+#python python/SKFlat.py -a ChargeFlip_IDv2 -y 2017 -i TTLL_powheg -n 50 --userflags HalfSampleTest &
+#python python/SKFlat.py -a ChargeFlip_IDv2 -y 2018 -i DYJets_MG -n 50 --userflags HalfSampleTest &
+#python python/SKFlat.py -a ChargeFlip_IDv2 -y 2018 -i TTLL_powheg -n 50 --userflags HalfSampleTest &
 
 #HalfSampleTest(second run), ScaleFactor, RunSyst using the above results
 
-#python python/SKFlat.py -a ChargeFlip -y 2016 -i DYJets -n 50 --userflags HalfSampleTest &
-#python python/SKFlat.py -a ChargeFlip -y 2016 -i DYJets_Pt-100To250 -n 50 --userflags HalfSampleTest &
-#python python/SKFlat.py -a ChargeFlip -y 2016 -i DYJets_Pt-250To400 -n 50 --userflags HalfSampleTest &
-#python python/SKFlat.py -a ChargeFlip -y 2016 -i DYJets_Pt-400To650 -n 50 --userflags HalfSampleTest &
-#python python/SKFlat.py -a ChargeFlip -y 2016 -i DYJets_Pt-650ToInf -n 50 --userflags HalfSampleTest &
-#python python/SKFlat.py -a ChargeFlip -y 2016 -i TTLL_powheg -n 50 --userflags HalfSampleTest &
-#python python/SKFlat.py -a ChargeFlip -y 2017 -i DYJets -n 50 --userflags HalfSampleTest &
-#python python/SKFlat.py -a ChargeFlip -y 2017 -i TTLL_powheg -n 50 --userflags HalfSampleTest &
-#python python/SKFlat.py -a ChargeFlip -y 2018 -i DYJets_MG -n 50 --userflags HalfSampleTest &
-#python python/SKFlat.py -a ChargeFlip -y 2018 -i TTLL_powheg -n 50 --userflags HalfSampleTest &
+#python python/SKFlat.py -a ChargeFlip_IDv2 -y 2016 -i DYJets -n 50 --userflags HalfSampleTest &
+#python python/SKFlat.py -a ChargeFlip_IDv2 -y 2016 -i TTLL_powheg -n 50 --userflags HalfSampleTest &
+#python python/SKFlat.py -a ChargeFlip_IDv2 -y 2017 -i DYJets -n 50 --userflags HalfSampleTest &
+#python python/SKFlat.py -a ChargeFlip_IDv2 -y 2017 -i TTLL_powheg -n 50 --userflags HalfSampleTest &
+#python python/SKFlat.py -a ChargeFlip_IDv2 -y 2018 -i DYJets_MG -n 50 --userflags HalfSampleTest &
+#python python/SKFlat.py -a ChargeFlip_IDv2 -y 2018 -i TTLL_powheg -n 50 --userflags HalfSampleTest &
 
-#python python/SKFlat.py -a ChargeFlip -y 2016 -i DoubleEG -n 10 --userflags ScaleFactor,RunSyst &
-#python python/SKFlat.py -a ChargeFlip -y 2017 -i DoubleEG -n 10 --userflags ScaleFactor,RunSyst &
-#python python/SKFlat.py -a ChargeFlip -y 2018 -i EGamma -n 10 --userflags ScaleFactor,RunSyst &
+#python python/SKFlat.py -a ChargeFlip_IDv2 -y 2016 -i DoubleEG -n 10 --userflags ScaleFactor,RunSyst &
+#python python/SKFlat.py -a ChargeFlip_IDv2 -y 2017 -i DoubleEG -n 10 --userflags ScaleFactor,RunSyst &
+#python python/SKFlat.py -a ChargeFlip_IDv2 -y 2018 -i EGamma -n 10 --userflags ScaleFactor,RunSyst &
+
+#ScaleFactor(second run) using the above results
+
+#python python/SKFlat.py -a ChargeFlip_IDv2 -y 2016 -i DoubleEG -n 10 --userflags ScaleFactor &
+#python python/SKFlat.py -a ChargeFlip_IDv2 -y 2017 -i DoubleEG -n 10 --userflags ScaleFactor &
+#python python/SKFlat.py -a ChargeFlip_IDv2 -y 2018 -i EGamma -n 10 --userflags ScaleFactor &
+
+
+###################################### etc ###############################################
+
+
+
+
+#python python/SKFlat.py -a ChargeFlip_IDv2 -y 2016 -i DYJets_Pt-100To250 -n 50 --userflags CFrate &
+#python python/SKFlat.py -a ChargeFlip_IDv2 -y 2016 -i DYJets_Pt-250To400 -n 50 --userflags CFrate &
+#python python/SKFlat.py -a ChargeFlip_IDv2 -y 2016 -i DYJets_Pt-400To650 -n 50 --userflags CFrate &
+#python python/SKFlat.py -a ChargeFlip_IDv2 -y 2016 -i DYJets_Pt-650ToInf -n 50 --userflags CFrate &
+#python python/SKFlat.py -a ChargeFlip_IDv2 -y 2016 -i DYJets_Pt-100To250 -n 50 --userflags ClosureTest &
+#python python/SKFlat.py -a ChargeFlip_IDv2 -y 2016 -i DYJets_Pt-250To400 -n 50 --userflags ClosureTest &
+#python python/SKFlat.py -a ChargeFlip_IDv2 -y 2016 -i DYJets_Pt-400To650 -n 50 --userflags ClosureTest &
+#python python/SKFlat.py -a ChargeFlip_IDv2 -y 2016 -i DYJets_Pt-650ToInf -n 50 --userflags ClosureTest &
+#python python/SKFlat.py -a ChargeFlip_IDv2 -y 2016 -i DYJets_Pt-100To250 -n 50 --userflags HalfSampleTest &
+#python python/SKFlat.py -a ChargeFlip_IDv2 -y 2016 -i DYJets_Pt-250To400 -n 50 --userflags HalfSampleTest &
+#python python/SKFlat.py -a ChargeFlip_IDv2 -y 2016 -i DYJets_Pt-400To650 -n 50 --userflags HalfSampleTest &
+#python python/SKFlat.py -a ChargeFlip_IDv2 -y 2016 -i DYJets_Pt-650ToInf -n 50 --userflags HalfSampleTest &
+#python python/SKFlat.py -a ChargeFlip_IDv2 -y 2016 -i DYJets_Pt-100To250 -n 50 --userflags HalfSampleTest &
+#python python/SKFlat.py -a ChargeFlip_IDv2 -y 2016 -i DYJets_Pt-250To400 -n 50 --userflags HalfSampleTest &
+#python python/SKFlat.py -a ChargeFlip_IDv2 -y 2016 -i DYJets_Pt-400To650 -n 50 --userflags HalfSampleTest &
+#python python/SKFlat.py -a ChargeFlip_IDv2 -y 2016 -i DYJets_Pt-650ToInf -n 50 --userflags HalfSampleTest &
