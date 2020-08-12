507ef5074c0fc6806272b4198f34544846709187
Hooooon12 <theking461@snu.ac.kr>
2020-08-12 10:19:42 +0900

    check highpt

--- Analyzers/src/HNtypeI_SR.C
+++ Analyzers/src/HNtypeI_SR.C
@@ -50,10 +50,12 @@ void HNtypeI_SR::initializeAnalyzer(){
   EMuTriggersH.clear();
 
   if(DataYear==2016){                                                                   // Lumi values for trigger weight (/pb)
-    MuonTriggers.push_back("HLT_Mu17_TrkIsoVVL_Mu8_TrkIsoVVL_v");                       // 27267.591112919 
-    MuonTriggers.push_back("HLT_Mu17_TrkIsoVVL_TkMu8_TrkIsoVVL_v");                     // 27267.591112919 //JH : NOTE these two are prescaled at 2016H -> https://its.cern.ch/jira/browse/CMSHLT-1002
-    MuonTriggers.push_back("HLT_Mu17_TrkIsoVVL_Mu8_TrkIsoVVL_DZ_v");                    // 35918.219492947
-    MuonTriggers.push_back("HLT_Mu17_TrkIsoVVL_TkMu8_TrkIsoVVL_DZ_v");                  // 35918.219492947 //JH : TODO why we need this though we already have superset
+    //MuonTriggers.push_back("HLT_Mu17_TrkIsoVVL_Mu8_TrkIsoVVL_v");                       // 27267.591112919 
+    //MuonTriggers.push_back("HLT_Mu17_TrkIsoVVL_TkMu8_TrkIsoVVL_v");                     // 27267.591112919 //JH : NOTE these two are prescaled at 2016H -> https://its.cern.ch/jira/browse/CMSHLT-1002
+    //MuonTriggers.push_back("HLT_Mu17_TrkIsoVVL_Mu8_TrkIsoVVL_DZ_v");                    // 35918.219492947
+    //MuonTriggers.push_back("HLT_Mu17_TrkIsoVVL_TkMu8_TrkIsoVVL_DZ_v");                  // 35918.219492947 //JH : TODO why we need this though we already have superset
+    MuonTriggers.push_back("HLT_Mu50_v");                    
+    MuonTriggers.push_back("HLT_TkMu50_v");                  //JH : to see how many events are cut with high pt
     MuonTriggersH.push_back("HLT_Mu17_TrkIsoVVL_Mu8_TrkIsoVVL_DZ_v");                   // 35918.219492947
     MuonTriggersH.push_back("HLT_Mu17_TrkIsoVVL_TkMu8_TrkIsoVVL_DZ_v");                 // 35918.219492947
     //MuonTriggers.push_back("HLT_Mu17_Mu8_SameSign_DZ_v");
@@ -74,7 +76,8 @@ void HNtypeI_SR::initializeAnalyzer(){
     //Mu23Ele12Triggers.push_back("HLT_Mu23_TrkIsoVVL_Ele8_CaloIdL_TrackIdL_IsoVL_v");
     //Mu23Ele12Triggers.push_back("HLT_Mu23_TrkIsoVVL_Ele8_CaloIdL_TrackIdL_IsoVL_DZ_v");
 
-    MuonPtCut1 = 20., MuonPtCut2 = 10.;
+    //MuonPtCut1 = 20., MuonPtCut2 = 10.;
+    MuonPtCut1 = 50., MuonPtCut2 = 50.; //JH : to see how many events are cut with high pt
     ElectronPtCut1 = 25., ElectronPtCut2 = 15.;
     EMuPtCut1 = 25., EMuPtCut2 = 15.;
     //EMuPtCut1 = 25., EMuPtCut2 = 10.;
@@ -565,9 +568,10 @@ void HNtypeI_SR::executeEventFromParameter(AnalyzerParameter param){
     if(!IsDATA){
       if(DataYear==2016){
         if(it_ch==0){
-          if(ev.PassTrigger(MuonTriggers)) dimu_trig_weight += 27267.591;
-          if(ev.PassTrigger(MuonTriggersH)) dimu_trig_weight += 8650.628; 
-          trigger_lumi = dimu_trig_weight;
+          //if(ev.PassTrigger(MuonTriggers)) dimu_trig_weight += 27267.591;
+          //if(ev.PassTrigger(MuonTriggersH)) dimu_trig_weight += 8650.628; 
+          //trigger_lumi = dimu_trig_weight;
+          trigger_lumi = ev.GetTriggerLumi("Full"); //JH : to see how many events are cut with high pt
         }
         if(it_ch==1) trigger_lumi = ev.GetTriggerLumi("Full");
         if(it_ch==2){
